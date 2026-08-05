1ce339d5f9316e4cf5e4c7a2fd64c5bbf48f04c7
Kronesta <151755423+Kronesta@users.noreply.github.com>
2026-08-05 09:59:44 +0200

    Update suchindex.js

--- suchindex.js
+++ suchindex.js
@@ -3,31 +3,113 @@ const suchIndex = [
 {
 titel:"Vermessung allgemein",
 seite:"grundlagen.html",
-begriffe:"Vermessung Vermessungstechniker Aufgaben Außendienst Innendienst"
+begriffe:"Vermessung Vermessungstechniker Aufgaben Außendienst Innendienst Messung Aufnahme Punkt Grenze Gebäude Fläche Karte Plan Dokumentation"
 },
 
 {
 titel:"Koordinatensysteme",
 seite:"grundlagen.html",
-begriffe:"UTM Gauß-Krüger EPSG ETRS89 Transformation"
+begriffe:"UTM Gauß-Krüger ETRS89 EPSG Lagebezug Höhenbezug Koordinaten Ostwert Nordwert Rechtswert Hochwert DHHN2016 NHN"
 },
+
+{
+titel:"GNSS und SAPOS",
+seite:"grundlagen.html",
+begriffe:"GNSS GPS Galileo GLONASS BeiDou Satellit Vermessung RTK NRTK SAPOS HEPS Korrekturdaten Genauigkeit Zentimeter"
+},
+
+{
+titel:"Transformationen",
+seite:"grundlagen.html",
+begriffe:"Transformation Verkettung Koordinatentransformation Bezugssystem Ausgangssystem Zielsystem EPSG UTM Gauß-Krüger"
+},
+
 
 {
 titel:"VermCAD",
 seite:"vermcad.html",
-begriffe:"VermCAD Berechnung Punkte Koordinaten Transformation"
+begriffe:"VermCAD Vermessung Projekt Punkt Datenimport Koordinaten Berechnung Konstruktion Zeichnung Auswertung Export MWT NV GF EV"
 },
+
+{
+titel:"Grenzpunkt bestimmen",
+seite:"grenzpunkt.html",
+begriffe:"Grenzpunkt bestimmen Punkt Grenzfeststellung Koordinate Passpunkt Vermessung Grenze"
+},
+
+{
+titel:"Grenzpunkt berichtigen",
+seite:"grenzpunkt_berichtigen.html",
+begriffe:"Grenzpunkt berichtigen Korrektur Koordinate Verschiebung Grenze Kataster"
+},
+
+{
+titel:"Gebäudepunkte",
+seite:"gebaeudepunkte.html",
+begriffe:"Gebäude Gebäudepunkt Aufnahme Bauwerk Punktmessung Vermessung"
+},
+
+{
+titel:"Vermessungsdokumentation",
+seite:"vermessungsdokumentation.html",
+begriffe:"Dokumentation Vermessung Nachweis Bericht Unterlagen Riss Messung Auswertung"
+},
+
 
 {
 titel:"DMS-Karibu Grundlagen",
 seite:"dmskaribu_grundlagen.html",
-begriffe:"DMS Karibu Dokumente Liegenschaftskataster Recherche"
+begriffe:"DMS Karibu Dokumentenmanagement Liegenschaftskataster Akten Verwaltung Recherche Vorgänge"
+},
+
+{
+titel:"DMS-Karibu Anmeldung",
+seite:"dmskaribu_anmeldung.html",
+begriffe:"Anmeldung Benutzer Nutzer Passwort Zugang Oberfläche Login"
 },
 
 {
 titel:"DMS-Karibu Oberfläche",
 seite:"dmskaribu_oberflaeche.html",
-begriffe:"Widgets Kopfleiste Kartenfenster Oberfläche"
+begriffe:"Oberfläche Widget Kopfleiste Seitenleiste Kartenfenster Suche Menü"
+},
+
+{
+titel:"DMS-Karibu Recherche",
+seite:"dmskaribu_recherche.html",
+begriffe:"Recherche Suche Dokument finden Akten Vorgang Flurstück"
+},
+
+{
+titel:"DMS-Karibu Dokumente",
+seite:"dmskaribu_dokumente.html",
+begriffe:"Dokumente öffnen verwalten speichern PDF Unterlagen"
+},
+
+{
+titel:"DMS-Karibu Geosieb",
+seite:"dmskaribu_geosieb.html",
+begriffe:"Geosieb Karte Flurstück Lage Darstellung Geometrie"
+},
+
+
+{
+titel:"CARLO",
+seite:"carlo.html",
+begriffe:"CARLO Vorgang Dokument Antrag Bearbeitung Kataster"
+},
+
+{
+titel:"GEOgraf",
+seite:"geograf.html",
+begriffe:"GEOgraf Grafik Zeichnung Plan Darstellung Vermessung"
+},
+
+
+{
+titel:"Glossar",
+seite:"glossar.html",
+begriffe:"Fachbegriffe Definition Erklärung Vermessung ALKIS SAPOS GNSS"
 }
 
 ];
