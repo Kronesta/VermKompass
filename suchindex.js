98f96d3d93febc32d3f7e9ce64a202735ad97d69
Kronesta <151755423+Kronesta@users.noreply.github.com>
2026-08-05 07:06:00 +0200

    Create suchindex.js

---
+++ suchindex.js
@@ -0,0 +1,33 @@
+const suchIndex = [
+
+{
+titel:"Vermessung allgemein",
+seite:"grundlagen.html",
+begriffe:"Vermessung Vermessungstechniker Aufgaben Außendienst Innendienst"
+},
+
+{
+titel:"Koordinatensysteme",
+seite:"grundlagen.html",
+begriffe:"UTM Gauß-Krüger EPSG ETRS89 Transformation"
+},
+
+{
+titel:"VermCAD",
+seite:"vermcad.html",
+begriffe:"VermCAD Berechnung Punkte Koordinaten Transformation"
+},
+
+{
+titel:"DMS-Karibu Grundlagen",
+seite:"dmskaribu_grundlagen.html",
+begriffe:"DMS Karibu Dokumente Liegenschaftskataster Recherche"
+},
+
+{
+titel:"DMS-Karibu Oberfläche",
+seite:"dmskaribu_oberflaeche.html",
+begriffe:"Widgets Kopfleiste Kartenfenster Oberfläche"
+}
+
+];
